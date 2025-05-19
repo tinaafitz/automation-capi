6b37f2235318c6903d31a266b700a6a54a19393c
Tina Fitzgerald <tfitzger@redhat.com>
2025-05-19 11:09:29 -0400

    Updated README and added create_OCP_vars.sh script. (#3)
    * Updated the readme and added the create_OCP_vars.sh script.
    
    * Forgot step to execute script.

---
+++ create_OCP_vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo ""
+echo "Setting OCP_HUB_API_URL, OCP_HUB_CLUSTER_USER and OCP_HUB_CLUSTER_PASSWORD env_vars."
+echo ""
+
+export OCP_HUB_API_URL=""
+export OCP_HUB_CLUSTER_USER=""
+export OCP_HUB_CLUSTER_PASSWORD=""
+
